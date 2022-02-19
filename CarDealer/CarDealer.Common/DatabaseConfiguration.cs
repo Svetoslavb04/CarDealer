d7040843212442b9b6403299b6ebf59a0c02874b
martinyovkov <martiyovkov10@gmail.com>
2022-02-19 13:10:10 +0200

    Change Password

--- CarDealer/CarDealer.Common/DatabaseConfiguration.cs
+++ CarDealer/CarDealer.Common/DatabaseConfiguration.cs
@@ -8,6 +8,6 @@ namespace CarDealer.Common
 {
     public class DatabaseConfiguration
     {
-        public static string DeffaultConnectionString = @"Server=.;Database=CarDealerITK;User Id=sa;Password=Whiskey10;";
+        public static string DeffaultConnectionString = @"Server=.;Database=CarDealerITK;User Id=sa;Password=Whiskey1;";
     }
 }
