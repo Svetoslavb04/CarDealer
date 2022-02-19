a9be9fb2a14ef77152544852fa3c46bcf7a48724
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2022-02-19 13:14:35 +0200

    das

--- CarDealer/CarDealer.Console/StartUp.cs
+++ CarDealer/CarDealer.Console/StartUp.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CarDealer.Common;
+using CarDealer.Data;
+using CarDealer.Data.Common;
+using CarDealer.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CarDealer.Console
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var services = new ServiceCollection()
+                .AddDbContext<CarDealerContext>(options =>
+                options.UseSqlServer(DatabaseConfiguration.DeffaultConnectionString))
+                .AddScoped<IRepository, CarDealerRepository>()
+                .BuildServiceProvider();
+
+            var database = services.GetService<IRepository>();
+
+            database.AddAsync<Car>(new Car
+            {
+                Make = "BMW",
+                Model = "X4",
+                Color = "white",
+                EngineCapacity= 5.2,
+                Horsepower = 100,
+                Year = 3
+            });
+            database.SaveChanges();
+            foreach (var car in database.All<Car>())
+            {
+                System.Console.WriteLine(car.Make);
+            }
+        }
+    }
+}
