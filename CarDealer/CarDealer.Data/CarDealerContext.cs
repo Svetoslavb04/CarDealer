ce2577b51325092d443a2a072005ab7915474bb8
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2022-02-19 12:31:16 +0200

    Created Console App

--- CarDealer/CarDealer.Data/CarDealerContext.cs
+++ CarDealer/CarDealer.Data/CarDealerContext.cs
@@ -1,11 +1,6 @@
 ﻿using CarDealer.Common;
 using CarDealer.Data.Models;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CarDealer.Data
 {
---
+++ CarDealer/CarDealer.Data/CarDealerRepository.cs
@@ -0,0 +1,17 @@
+﻿using CarDealer.Data.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDealer.Data
+{
+    public class CarDealerRepository : Repository
+    {
+        public CarDealerRepository(CarDealerContext context)
+        {   
+            this.Context = context;
+        }
+    }
+}
